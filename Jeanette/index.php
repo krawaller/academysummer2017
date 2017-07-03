cebb4a02fb7325cd560903e3775c28d60ccfd1a2
Jeanette Norin <jeanette.norin@hotmail.com>
2017-07-03 13:41:52 +0200

    Jeanette's fil

---
+++ Jeanette/index.php
@@ -0,0 +1,34 @@
+<?php
+/*Cecilia Lundqvist & Jeanette Norin*/
+date_default_timezone_set('Europe/Stockholm');
+include 'common.library.php';
+include 'header.php';
+?>
+
+  <div class="container">
+    <h1>JEANETTE NORIN</h1>
+    <h2>javautvecklare</h2>
+    <img src="ProfilePic.png" alt="Picture of Jeanette Norin" />
+
+    <table>
+      <tr>
+        <th>Skill</th><th>0</th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>10</th>
+      </tr>
+      <tr>
+        <td><p>HTML</p></td><td></td><td></td><td></td><td></td><td></td><td>o</td><td></td><td></td><td></td><td></td><td></td>
+      </tr>
+      <tr>
+        <td><p>CSS</p></td><td></td><td></td><td></td><td></td><td>o</td><td></td><td></td><td></td><td></td><td></td><td></td>
+      </tr>
+      <tr>
+        <td><p>JavaScript</p></td><td></td><td></td><td>o</td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+      </tr>
+      <tr>
+        <td><p>Tech-curiosity</p></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td>o</td><td></td><td></td>
+      </tr>
+
+    </table>
+  </div
+
+</body>
+</html>
