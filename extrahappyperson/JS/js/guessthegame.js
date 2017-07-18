9fc7f088b5c6e8d8268e6e9d72f91a6f775be71f
extrahappyperson <ht.uzpharma@gmail.com>
2017-07-18 16:06:36 +0200

    Guessing game done by FK

---
+++ extrahappyperson/JS/js/guessthegame.js
@@ -0,0 +1,50 @@
+
+console.log(answer);
+
+var randomNumber = Math.floor(Math.random()*10) + 1; 
+
+var numberofTries = 3;
+var tries=0;
+var y = "Yes"; 
+var n = "No"; 
+        
+    while(true){
+     
+    var answer = parseInt(prompt("Guess the number")); 
+        
+            
+    if(answer===randomNumber) {
+        alert("you guess it right"); 
+        break; 
+    }
+    
+    else{
+        tries++;
+        alert("try again" + " try number: " + tries); 
+        if(answer>randomNumber){
+            alert("go lower")
+        }
+        else {
+            alert("go higher")
+        }
+        
+        if (tries===3) {
+            var playAgain = prompt("Will you play again?"); 
+            if(playAgain===y){
+                tries=0;
+                continue; 
+            }
+                
+            else {
+                break;
+            }
+                
+        }
+        
+    }
+        
+}
+
+    
+
+
