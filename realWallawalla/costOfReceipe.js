1d54d784fc198f986eaf772ede9aa9a70ede6d2e
Your Name <you@example.com>
2017-07-19 14:00:53 +0200

    cost of recipe och cheapest cost

---
+++ realWallawalla/costOfReceipe.js
@@ -0,0 +1,73 @@
+
+var supersoup = {
+  potato: 3,
+  onion: 1,
+  corn: 1
+};
+
+var ICA = {
+  potato: 12,
+  tomato: 7,
+  cucumber: 3,
+  corn: 5,
+  onion: 80
+  // ...
+};
+
+var wallaStore = {
+  potato: 1,
+  tomato: 2,
+  cucumber: 3,
+  corn: 5,
+  onion: 1
+  // ...
+};
+
+var lindhl = {
+  potato: 120,
+  tomato: 17,
+  cucumber: 3,
+  corn: 550,
+  onion: 800
+  // ...
+};
+
+var shops = {
+  lindhl: lindhl,
+  wallaStore: wallaStore,
+  ICA: ICA
+}
+
+
+function costOfRecipie(recipe, store) {
+
+var totalCost = 0;
+
+  for (var key in recipe) {
+
+    var amount = recipe[key];
+    var price = store[key];
+    var cost = amount * price;
+    totalCost = totalCost + cost;
+    }
+    return totalCost;
+  }
+
+  function findCheapestStore(recipe, shops) {
+    var cheapest = 1000000000000;
+    var name;
+    for (var key in shops){
+      if (costOfRecipie(recipe ,shops[key])<cheapest) {
+          cheapest = costOfRecipie(recipe ,shops[key]);
+          name = key;
+      }
+
+    }
+  return name;
+  }
+
+  var totalCost = costOfRecipie(supersoup, ICA);
+  console.log(totalCost);
+
+  var cheapestName = findCheapestStore(supersoup, shops);
+  console.log(cheapestName);
